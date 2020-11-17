8f365e0fcf82dd504389ed67a241669156718e15
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-17 15:03:52 +0200

    Add simple validations

--- script.js
+++ script.js
@@ -10,9 +10,26 @@ const password  = document.getElementById('password');
  **************************/
 function showError(input,message){
  const formControl = input.parentElement;
+ formControl.className = "form_field error";
+ const small = formControl.querySelector('small')
+ small.innerText = message
+}
+/**************************
+  Show input success 
+ **************************/
+function showSucces(input){
+  const formControl = input.parentElement;
+  formControl.className = "form_field success";
 }
 
-
+/**************************
+  Check email validity 
+ **************************/
+function isValidEmail(email)
+{
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
 
 
 /**************************
@@ -20,12 +37,27 @@ function showError(input,message){
  **************************/
 form.addEventListener("submit",function(e){
   e.preventDefault();
- 
-if(email.value === ""){
-  showError(email,"Email is required");
-}else{
-  showSucces(email)
-}
+  
+  if(email.value === ""){
+    showError(email,"Email is required");
+  }
+  else if(!isValidEmail(email.value)) {
+    showError(email,"Email is not valid");
 
+  }else{
+    showSucces(email)
+  }
+
+  if(number.value === ""){
+    showError(number,"Number is required");
+  }else{
+    showSucces(number)
+  }
+
+  if(password.value === ""){
+    showError(password,"Password is required");
+  }else{
+    showSucces(password)
+  }
 
 });
